d0a085f3acfe4584ef60891629abb5a6b0125135
wendy.hsiao <wendy19940331@gmail.com>
2019-06-30 23:58:17 +0800

    Add todoSeeder

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ db.once('open', () => {
   console.log('mongodb connected!!!')
 })
 
+// 載入 todo model
 const Todo = require('./models/todo')
 
 app.get('/', (req, res) => {
---
+++ models/seeds/todoSeeder.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Todo = require('../todo')
+
+mongoose.connect('mongodb://127.0.0.1/todo', { useNewUrlParser: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('db error')
+})
+
+db.once('opne', () => {
+  console.log('db connected!')
+
+  for (var i = 0; i < 10; i++) {
+    Todo.create({ name: 'name-' + i })
+  }
+
+  console.log('done!')
+
+})
